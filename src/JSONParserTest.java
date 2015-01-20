cb11667524b7cf75d0ce5ea50663b6989e7a6d8a
JJ Wanda <jjwanda@gmail.com>
2015-01-20 01:23:39 -0500

    Created the beginnings of a test class

---
+++ src/JSONParserTest.java
@@ -0,0 +1,78 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class JSONParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testJSONParser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJSONParserString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadJSONFromUrlString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadJSONFromUrl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadJSONArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadJSONArrayString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSingle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPostRequestStringJSONObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPostRequestString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutJSONObjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteRequest() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/JSONTestClass.java
@@ -0,0 +1,4 @@
+
+public class JSONTestClass extends JSONParser<JSONTestClass> {
+
+}
